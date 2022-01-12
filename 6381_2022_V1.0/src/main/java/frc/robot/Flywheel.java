c344c6184895c4b1dc0d4190e6b4a005cdf18db3
redraider6381 <6381programming@gmail.com>
2022-01-11 19:53:38 -0600

    tt

--- 6381_2022_V1.0/src/main/java/frc/robot/Flywheel.java
+++ 6381_2022_V1.0/src/main/java/frc/robot/Flywheel.java
@@ -4,7 +4,11 @@ package frc.robot;
 public class Flywheel {
     
     public void SetSpeed(double Speed){
+<<<<<<< Updated upstream
      //   Flywheel.setVoltage(pid.calculate(Flywheel.get(), Speed) + feedforward);
+=======
+      //  Flywheel.setVoltage(pid.calculate(Flywheel.get(), Speed) + feedforward);
+>>>>>>> Stashed changes
 //use https://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/first/wpilibj/controller/ProfiledPIDController.html to make a PID, 
     }
 
